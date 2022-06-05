c1f33da3f540bb76aa5a1498345ffa095a5b1757
Maksym1os <maksym.los.knm.2020.@edu.lpnu.ua>
2022-06-05 17:17:09 +0300

    hiding login

---
+++ src/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:8000/'
+
+const API = {
+    // sendLoginRequest: (username, password) =>
+    //     axios.post('/login', {username, password}),
+    // sendSignupRequest: (username, password) =>
+    //     axios.post('/signup', {username, password}),
+    // sendEditUserRequest: (jwt, username, body) =>
+    //     axios.put(`/user?username=${username}`,
+    //         body, {
+    //             headers: {Authorization: `Bearer ${jwt}`}
+    //         }),
+    // sendDeleteUserRequest: (jwt, username) =>
+    //     axios.delete(`/user?username=${username}`,
+    //         {
+    //             headers: {Authorization: `Bearer ${jwt}`}
+    //         }),
+    getAllUsers: (jwt) =>
+        axios.get('users', {headers: {Authorization: `Bearer ${jwt}`}})
+}
+
+export default API;
